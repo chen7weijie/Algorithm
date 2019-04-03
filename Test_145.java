1d3d0bdca0803620b63d46623d44bc9352046924
chen7weijie <chen7weijie@163.com>
2019-04-03 11:29:23 +0800

    Leetcode_145 binary-tree-postorder-traversal

--- Test_145.java
+++ Test_145.java
@@ -4,6 +4,7 @@ import DataStructure.TreeNode;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 /*给定一个二叉树，返回它的 后序 遍历。
         示例:
@@ -34,5 +35,24 @@ public class Test_145 {
             postOrder(root.right);
         }
         list.add(root.val);
+    }
+    //迭代法，稍微改变先序遍历的迭代法，使得入栈顺序为根左右，然后改变list顺序
+    public List<Integer> postorderTraversal2(TreeNode root){
+        Stack<TreeNode> stack=new Stack<>();
+        if(root==null){
+            return list;
+        }
+        stack.push(root);
+       while (!stack.isEmpty()){
+           TreeNode node=stack.pop();
+           list.add(0,node.val);
+           if(node.left!=null){
+               stack.push(node.left);
+           }
+           if(node.right!=null){
+               stack.push(node.right);
+           }
+       }
+        return list;
     }
 }
