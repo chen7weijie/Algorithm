08d26aa39b2efe15c439d99cfe488e9a56923b41
chen7weijie <chen7weijie@163.com>
2019-04-04 16:11:38 +0800

    middle-of-the-linked-list

---
+++ Linkedlist/Test_876.java
@@ -0,0 +1,19 @@
+package Leetcode.Linkedlist;
+
+import DataStructure.ListNode;
+
+public class Test_876 {
+    public ListNode middleNode(ListNode head) {
+        if(head==null||head.next==null){
+            return head;
+        }
+        ListNode slow=head.next;
+        ListNode fast=head.next;
+        while (fast.next!=null&&fast.next.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+
+        }
+        return slow;
+    }
+}
