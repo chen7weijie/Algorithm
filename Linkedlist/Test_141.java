c519c18800ed56e04604dbe15aa2152818831e33
chen7weijie <chen7weijie@163.com>
2019-04-05 21:29:48 +0800

    Leetcode_141 linked-list-cycle

---
+++ Linkedlist/Test_141.java
@@ -0,0 +1,21 @@
+package Leetcode.Linkedlist;
+
+import DataStructure.ListNode;
+
+public class Test_141 {
+    public boolean hasCycle(ListNode head) {
+        if(head==null||head.next==null){
+            return false;
+        }
+        ListNode slow=head;
+        ListNode fast=head.next;
+        while (slow!=fast){
+            if(fast==null||fast.next==null){
+                return false;
+            }
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return true;
+    }
+}
