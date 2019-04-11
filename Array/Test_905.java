8d4649ddb2c56f2ad97865d49a3e77468de223db
chen7weijie <chen7weijie@163.com>
2019-04-11 23:28:14 +0800

    leetcode_905

---
+++ Array/Test_905.java
@@ -0,0 +1,20 @@
+package Leetcode.Array;
+
+public class Test_905 {
+    public int[] sortArrayByParity(int[] A) {
+        int[] result=new int[A.length];
+        int pre=0;
+        int last=A.length-1;
+        for(int i=0;i<A.length;i++){
+            if(A[i]%2==0){
+                result[pre]=A[i];
+                pre++;
+            }
+            else {
+                result[last]=A[i];
+                last--;
+            }
+        }
+        return result;
+    }
+}
