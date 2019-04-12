6c25950b4b2fa8f5dde22ca763b0f67eed1ccbec
chen7weijie <chen7weijie@163.com>
2019-04-12 16:45:49 +0800

    leetcode_561

---
+++ Array/Test_561.java
@@ -0,0 +1,14 @@
+package Leetcode.Array;
+
+import java.util.Arrays;
+
+public class Test_561 {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int result=0;
+        for(int i=0;i<nums.length;i+=2){
+            result+=nums[i];
+        }
+        return result;
+    }
+}
