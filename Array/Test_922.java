2bbe27c55253817695e9cb5442dff6424e9dd80e
chen7weijie <chen7weijie@163.com>
2019-04-12 16:34:37 +0800

    Leetcode_922

---
+++ Array/Test_922.java
@@ -0,0 +1,20 @@
+package Leetcode.Array;
+
+public class Test_922 {
+    public int[] sortArrayByParityII(int[] A) {
+        int[] result=new int[A.length];
+        int even=0;
+        int odd=1;
+        for (int i=0;i<A.length;i++){
+            if(A[i]%2==0){
+                result[even]=A[i];
+                even+=2;
+            }
+            else {
+                result[odd]=A[i];
+                odd+=2;
+            }
+        }
+        return result;
+    }
+}
