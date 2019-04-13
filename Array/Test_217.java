b799c6978922c2d7ddac44ce78dbd08ad8db5771
chen7weijie <chen7weijie@163.com>
2019-04-13 22:25:39 +0800

    Leetcode_217

---
+++ Array/Test_217.java
@@ -0,0 +1,15 @@
+package Leetcode.Array;
+
+import java.util.Arrays;
+
+public class Test_217 {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]==nums[i+1]){
+                return true;
+            }
+        }
+        return false;
+    }
+}
