f0a9bccbe9358a812df67aa25e6d69a806e1cbc5
chen7weijie <chen7weijie@163.com>
2019-04-14 23:41:17 +0800

    Leetcode_53

---
+++ Array/Test_53.java
@@ -0,0 +1,21 @@
+package Leetcode.Array;
+
+public class Test_53 {
+    public int maxSubArray(int[] nums) {
+        int[] sum=new int[nums.length];
+        sum[0]=nums[0];
+        int max=nums[0];
+        for(int i=1;i<nums.length;i++){
+            if(sum[i-1]<0){
+                sum[i]=nums[i];
+            }
+            else{
+                sum[i]=sum[i-1]+nums[i];
+            }
+            if(sum[i]>max){
+                max=sum[i];
+            }
+        }
+        return max;
+    }
+}
