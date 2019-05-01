0430300a380ad6d1a1c3d9f56f0f8e09a6c3b8ee
chen7weijie <chen7weijie@163.com>
2019-05-01 20:48:38 +0800

    leetcode 804

---
+++ String/UniqueMorseRepresentations.java
@@ -0,0 +1,46 @@
+package Leetcode.String;
+
+import java.util.HashSet;
+
+public class UniqueMorseRepresentations {
+    public int uniqueMorseRepresentations(String[] words) {
+        String[] map = {
+                ".-",
+                "-...",
+                "-.-.",
+                "-..",
+                ".",
+                "..-.",
+                "--.",
+                "....",
+                "..",
+                ".---",
+                "-.-",
+                ".-..",
+                "--",
+                "-.",
+                "---",
+                ".--.",
+                "--.-",
+                ".-.",
+                "...",
+                "-",
+                "..-",
+                "...-",
+                ".--",
+                "-..-",
+                "-.--",
+                "--.."
+        };
+        if (words == null) return 0;
+        HashSet<String> set = new HashSet<String>();
+        for (String s : words) {
+            StringBuilder sb = new StringBuilder();
+            for (char c : s.toCharArray()) {
+                sb.append(map[c - 'a']);
+            }
+            set.add(sb.toString());
+        }
+        return set.size();
+    }
+}
